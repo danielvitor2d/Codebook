9d5151383db501d07345bc4d6b0b545c7e9702d0
Daniel Vitor <ripsea.rpg321@gmail.com>
2021-02-27 17:21:49 -0300

    feat: update suffix array

--- code/strings/suffix_array.h
+++ code/strings/suffix_array.h
@@ -1,89 +1,100 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-vector<int> sort_cyclic_shifts(const string &s) {
-	int n = s.size();
-	const int alphabet = 256;
-
-	vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
-	for (int i = 0; i < n; ++i)
-		++cnt[s[i]];
-	for (int i = 1; i < alphabet; ++i)
-		cnt[i] += cnt[i-1];
-	for (int i = 0; i < n; ++i)
-		p[--cnt[s[i]]] = i;
-	c[p[0]] = 0;
-	int classes = 1;
-	for (int i = 1; i < n; ++i) {
-		if (s[p[i]] != s[p[i-1]])
-			++classes;
-		c[p[i]] = classes - 1;
+#include <numeric>
+#include <vector>
+#include <string>
+typedef std::pair<int, int> ii;
+class SuffixArray {
+	std::vector<int> RA, SA, tempRA, tempSA, c;
+	std::vector<int> LCP, Phi, PLCP;
+	std::string S;
+	int N;
+	void countingSort(int k) {
+		int sum = 0, maxi = std::max(256, N);
+		c.assign(maxi, 0);
+		for (int i = 0; i < N; ++i)
+			c[RA[(i+k)%N]]++;
+		for (int i = 1; i < maxi; ++i)
+			c[i] += c[i-1];
+		for (int i = N-1; i >= 0; --i)
+			tempSA[--c[RA[(SA[i]+k)%N]]] = SA[i];
+		SA = tempSA;
 	}
-	
-	vector<int> pn(n), cn(n);
-	for (int h = 0; (1 << h) < n; ++h) {
-		int h2 = (1 << h);
-		for (int i = 0; i < n; ++i) {
-			pn[i] = p[i] - h2;
-			if (pn[i] < 0) pn[i] += n;
+	void constructSA() {
+		iota(SA.begin(), SA.end(), 0);
+		for (int i = 0; i < N; ++i) RA[i] = S[i];
+		for (int k = 1; k < N; k <<= 1) {
+			countingSort(k);
+			countingSort(0);
+			int r = 0;
+			tempRA[SA[0]] = 0;
+			for (int i = 1; i < N; ++i) {
+				tempRA[SA[i]] = 
+					(ii(RA[SA[i]], RA[(SA[i]+k)%N]) == ii(RA[SA[i-1]], RA[(SA[i-1]+k)%N]) ? r : ++r);
+			}
+			RA = tempRA;
+			if (RA[SA[N-1]] == N-1) break;
 		}
-		fill(cnt.begin(), cnt.begin()+classes, 0);
-		for (int i = 0; i < n; ++i)
-			++cnt[c[pn[i]]];
-		for (int i = 1; i < classes; ++i)
-			cnt[i] += cnt[i-1];
-		for (int i = n-1; i >= 0; --i)
-			p[--cnt[c[pn[i]]]] = pn[i];
-		cn[p[0]] = 0;
-		classes = 1;
-		for (int i = 1; i < n; ++i) {
-			pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
-			pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
-			if (cur != prev)
-				++classes;
-			cn[p[i]] = classes - 1;
-		}
-		c.swap(cn);
 	}
-	return p;
-}
-
-/*
-	Run in O(|s|*log(|s|))
-*/
-vector<int> suffix_array_construction(string s) {
-	s += '$';
-	vector<int> sorted_shifts = sort_cyclic_shifts(s);
-	sorted_shifts.erase(sorted_shifts.begin());
-	return sorted_shifts;
-}
-
-/*
-	Kasai's algorithm run in O(n)
-*/
-vector<int> lcp_construction(const string &s, vector<int> &p) {
-	int n = (int)s.size();
-	vector<int> rank(n, 0);
-	for (int i = 0; i < n; ++i) {
-		rank[p[i]] = i;
-	}
-	int k = 0;
-	vector<int> lcp(n-1, 0);
-	for (int i = 0; i < n; ++i) {
-		if (rank[i] == n-1) {
-			k = 0;
-			continue;
+	void constructLCP() {
+		Phi[SA[0]] = -1;
+		for (int i = 1; i < N; ++i) {
+			Phi[SA[i]] = SA[i-1];
 		}
-		int j = p[rank[i] + 1];
-		while (i + k < n and j + k < n and s[i+k] != s[j+k]) {
-			++k;
+		for (int i = 0, k = 0; i < N; ++i) {
+			if (Phi[i] == -1) { PLCP[i] = 0; continue; }
+			while (S[i+k] == S[Phi[i]+k]) ++k;
+			PLCP[i] = k;
+			k = std::max(k-1, 0);
 		}
-		lcp[rank[i]] = k;
-		if (k) --k;
+		for (int i = 0; i < N; ++i)
+			LCP[i] = PLCP[SA[i]];
 	}
-	return lcp;
-}
-/*
-	Number of different substrings:
-	(n^2+n)/2 - sum_{i=0 to n-2} lcp[i]
-*/
+	ii stringMatching(const std::string &s) {
+		int m = s.size();
+		int lo = 0, hi = N-2, mid;
+		while (lo < hi) { /*Find the lower bound*/
+			mid = lo+(hi-lo)/2;
+			if (S.substr(SA[mid], m) >= s) hi = mid;
+			else lo = mid + 1;
+		}
+		if (S.substr(SA[lo], m) != s) return {-1, -1};
+		ii ans = {lo, lo};
+		lo = 0, hi = N-2;
+		while (lo < hi) { /*Find the upper bound*/
+			mid = lo+(hi-lo)/2;
+			if (S.substr(SA[mid], m) > s) hi = mid;
+			else lo = mid + 1;
+		}
+		if (S.substr(SA[lo], m) != s) --hi;
+		ans.second = hi;
+		return ans;
+	}
+public:
+	SuffixArray (const std::string &s) : S(s) {
+		S += '$';
+		N = S.size();
+		RA.assign(N, 0);
+		SA.assign(N, 0);
+		tempSA.assign(N, 0);
+		tempRA.assign(N, 0);
+		LCP.assign(N, 0);
+		PLCP.assign(N, 0);
+		Phi.assign(N, 0);
+		constructSA();
+		constructLCP();
+		SA.erase(SA.begin());
+		LCP.erase(LCP.begin());
+	}
+	std::vector<int> getSA() {
+		return SA;
+	}
+	std::vector<int> getLCP() {
+		return LCP;
+	}
+	ii getStringMatching(const std::string &s) {
+		return stringMatching(s);
+	}
+	/*
+		Number of different substrings:
+		(n^2+n)/2 - sum_{i=0 to n-2} lcp[i]
+	*/
+};
