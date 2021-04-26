10535590b1a2f4885121944d7859d73eb6ae77a2
Daniel Vitor <ripsea.rpg321@gmail.com>
2021-04-26 20:30:28 -0300

    feat: fix bug in Modular Arithmetic

--- code/math/modular.h
+++ code/math/modular.h
@@ -22,9 +22,9 @@ int64_t modDiv(int64_t a, int64_t b, int64_t mod = MOD) {
     return modMul(a, inv_mod(b, mod), mod);
 }
 //O(log(a))
-int64_t bigModMul(int64_t a, int64_t b) {
+int64_t bigModMul(int64_t a, int64_t b, int64_t mod = MOD) {
 	int64_t ans = 0LL;
-	b %= MOD;
+	b %= mod;
 	while (a > 0) {
 		if (a & 1) ans = modSum(ans, b);
 		b = modMul(b, 2LL);
@@ -32,14 +32,14 @@ int64_t bigModMul(int64_t a, int64_t b) {
 	}
 	return ans;
 }
-uint64_t bigModMul_2(uint64_t a, uint64_t b) {
+uint64_t bigModMul_2(uint64_t a, uint64_t b, int64_t mod = MOD) {
 	long double x;
 	uint64_t c;
 	int64_t r;
-	if (a >= MOD) a %= MOD;
-	if (b >= MOD) b %= MOD;
+	if (a >= mod) a %= mod;
+	if (b >= mod) b %= mod;
 	x = a;
-	c = (x * b) / MOD;
-	r = (int64_t) (a * b - c * MOD) % (int64_t)MOD;
-	return (r < 0 ? r+MOD : r);
+	c = (x * b) / mod;
+	r = (int64_t) (a * b - c * mod) % (int64_t)mod;
+	return (r < 0 ? r+mod : r);
 }
